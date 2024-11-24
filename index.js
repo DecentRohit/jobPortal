cbd040e4865e7bf9bf050e5b35d40d9d84d455b0
Rohit Soni <royrohit848@gmail.com>
2024-11-24 21:19:36 +0530

    patch

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import router from "./src/Routes/HomeRoutes.js";
 import path from "path";
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
+import expressLayouts from 'express-ejs-layouts';
 
 const app  = express();
 
@@ -21,7 +22,10 @@ app.set('view engine', 'ejs');
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(expressLayouts);
+app.set('layout', 'layout'); // Points to `views/layout.ejs`
+app.set("layout extractStyles" , true); //tell ejs where to put <link or script tag when encountered in pages
+app.set("layout extractScripts" , true);
 app.use('/' , router);
 
 
