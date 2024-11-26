c6fe2a5a38e9044c65509bf30e275034b81f7d89
Rohit Soni <royrohit848@gmail.com>
2024-11-26 15:02:33 +0530

    Stop tracking .env file

--- src/middlewares/nodemailer.js
+++ src/middlewares/nodemailer.js
@@ -6,3 +6,25 @@
 // JsonWebTokenError
 // session cookieParser
 // all Controller
+import nodemailer from 'nodemailer';
+
+const userEmail = 'todo'
+const transporter = nodemailer.createTransport({
+
+    service : 'gmail' ,
+    auth : {
+        user : 'royrohit848@gmail.com' ,
+        pass : process.env.PASS
+    }
+})
+
+const mailOptions = {
+    from : 'royrohit848@gmail.com' ,
+    to : userEmail ,
+    subject : 'Applied for new Job',
+    text : 'Job application has been registered with us'
+}
+
+const sendMail = transporter.sendMail(mailOptions);
+
+export default sendMail;
