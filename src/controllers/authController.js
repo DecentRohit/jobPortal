8e29e48ffe18c201615ca3abc5cf07ed0b475232
Rohit Soni <royrohit848@gmail.com>
2024-12-05 16:33:59 +0530

    success and errormsg not getting inside locals

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -10,72 +10,72 @@ import UserModel from "../models/userSchema.js"
 // import Swal from 'sweetalert2/dist/sweetalert2.js'
 
 
-export const login = async(req, res , next)=>{
-  try{
-    const {email, password} = req.body;
-    const user =  await UserModel.findOne({email});
-    if(user){
-        const passwordMatched = await bcrypt.compare(password , user.password)
-        if(passwordMatched){
-         const userName = user.name;
-         const userID = user._id ;
-         const userEmail = user.email;
-        
-        
-          req.session.userInfo = {userID , userName , userEmail};
-          
-           req.flash('Success', 'Welcome to the website!');
-                  
-          res.redirect('/jobs')
-        }else{
-         res.render('userNotFound')
-        }
-    }else{
-        res.render('userNotFound')
-    }
-   
-  } catch(err){
-    console.log(err)
-    res.render('somethingWentWrong')
-  }
-   
-   
-    }
-    export const logout = async(req, res)=>{
-     await   req.session.destroy((err)=>{
-            console.log("logged out successfully")
-            if(err){
-                console.log(err)
-                res.end("failed to logout ")
-            }
-        })
+export const login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        const user = await UserModel.findOne({ email });
+        if (user) {
+            const passwordMatched = await bcrypt.compare(password, user.password)
+            if (passwordMatched) {
+                const userName = user.name;
+                const userID = user._id;
+                const userEmail = user.email;
 
-        await res.clearCookie('lastVisit')
-        res.redirect('/')
-        
-    }
-    export const register = async(req, res)=>{
-        const {name , email , password } = req.body;
-        const user = await UserModel.findOne({email});
-        if(user){
-        console.log("user already exists")
-        }else{
-            const hashpassword = await bcrypt.hash(password , 12)
-            const newRecruiter = await UserModel.create({name, email , password : hashpassword})
-            await   newRecruiter.save();
+
+                req.session.userInfo = { userID, userName, userEmail };
+
+                req.flash('success' , "hello")
+                console.log(res.locals)
+
+                res.redirect('/jobs')
+            } else {
+                res.render('userNotFound')
+            }
+        } else {
+            res.render('userNotFound')
         }
-       
-       res.redirect('/loginPage' )
+
+    } catch (err) {
+        console.log(err)
+        res.render('somethingWentWrong')
     }
-    
-    export const loginPage = async (req, res)=>{
-        res.render("login" )
+
+
+}
+export const logout = async (req, res) => {
+    await req.session.destroy((err) => {
+        console.log("logged out successfully")
+        if (err) {
+            console.log(err)
+            res.end("failed to logout ")
+        }
+    })
+
+    await res.clearCookie('lastVisit')
+    res.redirect('/')
+
+}
+export const register = async (req, res) => {
+    const { name, email, password } = req.body;
+    const user = await UserModel.findOne({ email });
+    if (user) {
+        console.log("user already exists")
+    } else {
+        const hashpassword = await bcrypt.hash(password, 12)
+        const newRecruiter = await UserModel.create({ name, email, password: hashpassword })
+        await newRecruiter.save();
     }
-     
-    export const home = async(req, res)=>{
-       
-          
-       
-        res.render("home" , {userInfo : req.session.userInfo})
-    }
- 
+
+    res.redirect('/loginPage')
+}
+
+export const loginPage = async (req, res) => {
+    res.render("login")
+}
+
+export const home = async (req, res) => {
+
+
+
+    res.render("home", { userInfo: req.session.userInfo })
+}
